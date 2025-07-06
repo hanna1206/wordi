6de922ffded395e41089498e92c2de1238995fc4
Hanna Kyriukhina <anne.snab+hanna1206@gmail.com>
2025-07-06 19:00:23 +0200

    fix: remove unused page

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,5 +1,9 @@
 import { UserSettings } from '@/modules/user-settings/pages/user-profile.page';
 
+// Force dynamic rendering since this page uses cookies for authentication
+// for initial loading
+export const dynamic = 'force-dynamic';
+
 export default function UserPage() {
   return <UserSettings />;
 }
