c8d21e4c76321cbc8250507a7298e2bdfd15660d
Hanna Kyriukhina <anne.snab+hanna1206@gmail.com>
2025-07-07 17:42:16 +0200

    feat: add AppHeader component and integrate it

--- src/modules/words/pages/create-word-page.tsx
+++ src/modules/words/pages/create-word-page.tsx
@@ -1,11 +1,15 @@
 import { Center, Container } from '@chakra-ui/react';
 
+import { AppHeader } from '@/components/app-header';
 import { CreateWordForm } from '@/modules/words/components/create-word-form';
 
 export const CreateWordPage = () => (
-  <Center h="100vh" bg="white" _dark={{ bg: 'gray.900' }}>
-    <Container maxW="4xl" w="full">
-      <CreateWordForm />
-    </Container>
-  </Center>
+  <>
+    <AppHeader />
+    <Center h="100vh" bg="white" _dark={{ bg: 'gray.900' }}>
+      <Container maxW="4xl" w="full">
+        <CreateWordForm />
+      </Container>
+    </Center>
+  </>
 );
