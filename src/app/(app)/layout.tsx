d999ac1e6e432ec67b25cdae60403bb0f20f1cb3
Hanna Kyriukhina <anne.snab+hanna1206@gmail.com>
2025-11-10 17:39:13 +0100

    refactor: enable dynamic rendering for app routes

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,5 +1,7 @@
 import { OnboardingRedirect } from '@/modules/user-settings/components/onboarding-redirect';
 
+export const dynamic = 'force-dynamic';
+
 const AppLayout = async ({ children }: { children: React.ReactNode }) => {
   return <OnboardingRedirect>{children}</OnboardingRedirect>;
 };
