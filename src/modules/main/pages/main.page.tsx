181ac37b9f3100d6e55c9b4aaf94e4960cb657b0
Hanna Kyriukhina <anne.snab+hanna1206@gmail.com>
2025-12-02 10:14:18 +0100

    feat(main): refactor MainPage to use Suspense for loading state and improve structure

--- src/modules/main/pages/main.page.tsx
+++ src/modules/main/pages/main.page.tsx
@@ -1,10 +1,12 @@
-import { Center, Container } from '@chakra-ui/react';
+import { Suspense } from 'react';
+
+import { Center, Container, Spinner } from '@chakra-ui/react';
 
 import { getUserCollections } from '@/modules/collection/collections.actions';
 import type { CollectionWithCount } from '@/modules/collection/collections.types';
 import { GenerateLinguisticItemForm } from '@/modules/linguistics/components/generate-linguistic-item-form';
 
-export const MainPage = async () => {
+const MainPageContent = async () => {
   let collections: CollectionWithCount[] = [];
   const collectionsResult = await getUserCollections();
 
@@ -12,10 +14,22 @@ export const MainPage = async () => {
     collections = collectionsResult.data;
   }
 
+  return <GenerateLinguisticItemForm collections={collections} />;
+};
+
+export const MainPage = () => {
   return (
     <Center h="full">
       <Container maxW="4xl" w="full">
-        <GenerateLinguisticItemForm collections={collections} />
+        <Suspense
+          fallback={
+            <Center h="200px">
+              <Spinner size="xl" />
+            </Center>
+          }
+        >
+          <MainPageContent />
+        </Suspense>
       </Container>
     </Center>
   );
